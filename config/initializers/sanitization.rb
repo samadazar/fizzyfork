6bf61b631e11aa6396d8679b968bec75505dbbfe
Mike Dalessio <mike@37signals.com>
2025-08-08 13:50:23 -0400

    Ensure sanitizer config applies to ActionText in production
    In production, eager loading prevents the default sanitizer config
    from applying to Action Text. Let's explicitly set it.
    
    ref: https://fizzy.37signals.com/5986089/collections/2/cards/1123

--- config/initializers/sanitization.rb
+++ config/initializers/sanitization.rb
@@ -1,4 +1,7 @@
 Rails.application.config.after_initialize do
   Rails::HTML5::SafeListSanitizer.allowed_tags.merge(%w[ s table tr td th thead tbody details summary video source])
   Rails::HTML5::SafeListSanitizer.allowed_attributes.merge(%w[ data-turbo-frame controls type width data-action data-lightbox-target data-lightbox-url-value ])
+
+  ActionText::ContentHelper.allowed_tags = Rails::HTML5::SafeListSanitizer.allowed_tags
+  ActionText::ContentHelper.allowed_attributes = Rails::HTML5::SafeListSanitizer.allowed_attributes
 end
